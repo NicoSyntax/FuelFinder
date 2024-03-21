11965e1abde4940b98481dbedf06578a0e14a93f
NicoSyntax <148044680+NicoSyntax@users.noreply.github.com>
2024-03-21 16:04:45 +0100

    Funktionen eingefügt, sodass beim oncreate view der api call mit der current location ausgeführt wird

--- app/src/main/java/de/syntax/androidabschluss/ui/HomeFragment.kt
+++ app/src/main/java/de/syntax/androidabschluss/ui/HomeFragment.kt
@@ -1,9 +1,11 @@
 package de.syntax.androidabschluss.ui
 
+
 import android.Manifest
 import android.content.Context
 import android.content.pm.PackageManager
 import android.location.Location
+import android.location.LocationListener
 import android.location.LocationManager
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -13,30 +15,51 @@ import android.widget.Toast
 import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
-import androidx.navigation.fragment.findNavController
-import de.syntax.androidabschluss.R
 import de.syntax.androidabschluss.adapter.HomeAdapter
 import de.syntax.androidabschluss.databinding.FragmentHomeBinding
-import de.syntax.androidabschluss.viewmodel.FirebaseViewModel
 import de.syntax.androidabschluss.viewmodel.MainViewModel
 
 class HomeFragment : Fragment() {
-    private val viewModel: FirebaseViewModel by activityViewModels()
+
+
+    private lateinit var locationManager: LocationManager
     private val mainViewModel: MainViewModel by activityViewModels()
     private lateinit var binding: FragmentHomeBinding
-    private lateinit var userMail: String
+
+    private val locationListener: LocationListener = object : LocationListener {
+        override fun onLocationChanged(location: Location) {
+
+
+            val latitude = location.latitude
+            val longitude = location.longitude
+            mainViewModel.loadStationsWithLocation(latitude, longitude)
+
+        }
+
+        override fun onStatusChanged(provider: String?, status: Int, extras: Bundle?) {}
+
+        override fun onProviderEnabled(provider: String) {}
+
+        override fun onProviderDisabled(provider: String) {}
+    }
+
     private val permissionRequestCode = 1
-    private lateinit var locationManager: LocationManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        locationManager = requireContext().getSystemService(Context.LOCATION_SERVICE) as LocationManager
-
-        if (ActivityCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-            requestLocation()
+        locationManager =
+            requireContext().getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        if (ActivityCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            requestLocationUpdates()
         } else {
-            requestPermissions(arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), permissionRequestCode)
+            requestPermissions(
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                permissionRequestCode
+            )
         }
     }
 
@@ -45,7 +68,6 @@ class HomeFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        requestLocation()
         binding = FragmentHomeBinding.inflate(inflater, container, false)
         return binding.root
     }
@@ -53,47 +75,46 @@ class HomeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        viewModel.currentUser.observe(viewLifecycleOwner) { user ->
-            if (user == null) {
-                findNavController().navigate(R.id.loginFragment)
-            } else {
-                userMail = user.email.toString()
-            }
+        mainViewModel.stations.observe(viewLifecycleOwner){
+            binding.rvStations.adapter = HomeAdapter(it.stations)
         }
-        mainViewModel.stations.observe(viewLifecycleOwner){ station ->
-            binding.rvStations.adapter = HomeAdapter(station.stations.sortedByDescending { it.isOpen })
-            binding.rvStations.setHasFixedSize(true)
-        }
+        binding.rvStations.setHasFixedSize(true)
     }
 
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+    override fun onDestroy() {
+        super.onDestroy()
+        locationManager.removeUpdates(locationListener)
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
         if (requestCode == permissionRequestCode && grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-
-            requestLocation()
+            requestLocationUpdates()
         } else {
-
-            Toast.makeText(requireContext(), "Location permission denied", Toast.LENGTH_SHORT).show()
+            Toast.makeText(requireContext(), "Location permission denied", Toast.LENGTH_SHORT)
+                .show()
         }
     }
 
-    private fun requestLocation() {
+    private fun requestLocationUpdates() {
         try {
-            val location: Location? = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER)
-            if (location != null) {
-
-                val latitude = location.latitude
-                val longitude = location.longitude
-
-                mainViewModel.loadStationsWithLocation(latitude, longitude)
-
-
-                Toast.makeText(requireContext(), "Latitude: $latitude, Longitude: $longitude", Toast.LENGTH_SHORT).show()
-            } else {
-                Toast.makeText(requireContext(), "Location not available", Toast.LENGTH_SHORT).show()
-            }
+            locationManager.requestLocationUpdates(
+                LocationManager.GPS_PROVIDER,
+                MIN_TIME_FOR_UPDATE,
+                MIN_DISTANCE_CHANGE,
+                locationListener
+            )
         } catch (e: SecurityException) {
-
-            Toast.makeText(requireContext(), "Location permission not granted", Toast.LENGTH_SHORT).show()
+            Toast.makeText(requireContext(), "Location permission not granted", Toast.LENGTH_SHORT)
+                .show()
         }
+    }
+
+    companion object {
+        private const val MIN_TIME_FOR_UPDATE: Long = 1000 * 60 * 1
+        private const val MIN_DISTANCE_CHANGE: Float = 10f
     }
 }
--- app/src/main/java/de/syntax/androidabschluss/ui/SettingsFragment.kt
+++ app/src/main/java/de/syntax/androidabschluss/ui/SettingsFragment.kt
@@ -1,34 +1,16 @@
-import android.Manifest
-import android.content.Context
-import android.content.pm.PackageManager
-import android.location.Location
-import android.location.LocationManager
+package de.syntax.androidabschluss.ui
+
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
-import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
 import de.syntax.androidabschluss.databinding.FragmentSettingsBinding
 
 class SettingsFragment : Fragment() {
 
-    private val permissionRequestCode = 1
-    private lateinit var locationManager: LocationManager
+
     private lateinit var binding: FragmentSettingsBinding
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-        locationManager = requireContext().getSystemService(Context.LOCATION_SERVICE) as LocationManager
-
-        if (ActivityCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-            requestLocation()
-        } else {
-            requestPermissions(arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), permissionRequestCode)
-        }
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -39,39 +21,12 @@ class SettingsFragment : Fragment() {
         return binding.root
     }
 
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
-        if (requestCode == permissionRequestCode && grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
 
-            requestLocation()
-        } else {
 
-            Toast.makeText(requireContext(), "Location permission denied", Toast.LENGTH_SHORT).show()
-        }
-    }
-
-    private fun requestLocation() {
-        try {
-            val location: Location? = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER)
-            if (location != null) {
-
-                val latitude = location.latitude
-                val longitude = location.longitude
-
-                Toast.makeText(requireContext(), "Latitude: $latitude, Longitude: $longitude", Toast.LENGTH_SHORT).show()
-            } else {
-                Toast.makeText(requireContext(), "Location not available", Toast.LENGTH_SHORT).show()
-            }
-        } catch (e: SecurityException) {
-
-            Toast.makeText(requireContext(), "Location permission not granted", Toast.LENGTH_SHORT).show()
-        }
-    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.getLocation.setOnClickListener {
-            requestLocation()
-        }
+
     }
 }
